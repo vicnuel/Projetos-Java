97d452a9d72e0d641c056b603ccadf9b342bfa5c
Victor Emanuel Sousa Lima <ve49550@gmail.com>
2021-03-01 09:47:14 -0300

    for

---
+++ respeticaoForWhile/src/respeticaoforwhile/RespeticaoForWhile.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package respeticaoforwhile;
+
+/**
+ *
+ * @author ve495
+ */
+public class RespeticaoForWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 1; i<=10; i++){
+            System.out.println(Integer.toString(i));
+        }
+        for (int i = 1; i<=10; ++i){
+            System.out.println(Integer.toString(i));
+        }
+    }
+    
+}
